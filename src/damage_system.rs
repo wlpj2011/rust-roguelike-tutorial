0f3593bfa2cb6db376e40216ba1ab37c36964937
William Johnson <wlpj2011@gmail.com>
2025-11-25 18:34:03 -0500

    Implemented Game Over

--- src/damage_system.rs
+++ src/damage_system.rs
@@ -1,4 +1,4 @@
-use super::{CombatStats, Name, Player, SufferDamage, gamelog::GameLog};
+use super::{CombatStats, Name, Player, SufferDamage, gamelog::GameLog, RunState};
 use specs::prelude::*;
 
 pub struct DamageSystem {}
@@ -39,7 +39,10 @@ pub fn delete_the_dead(ecs: &mut World) {
                         }
                         dead.push(entity)
                     }
-                    Some(_) => log.entries.push("You are dead".to_string()),
+                    Some(_) => {
+                        let mut runstate = ecs.write_resource::<RunState>();
+                        *runstate = RunState::GameOver;
+                    }
                 }
             }
         }
